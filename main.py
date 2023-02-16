312fde279ed8cd6095e977359dc1c57192bcd3a8
0931REB462 <125568233+0931REB462@users.noreply.github.com>
2023-02-16 14:24:57 +0000

    -

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # python3
 
 from collections import namedtuple
+from turtle import position
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
@@ -12,18 +13,27 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
+        # enumerate - skaitītājs, simbols
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, position + 1))
+            
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1], next):
+                print(i+1)
+            opening_brackets_stack.pop()
+    if not opening_brackets_stack:
+        print("Success")
+    else:
+        print(opening_brackets_stack[-1]. position)
+
 
 
 def main():
     text = input()
-    mismatch = find_mismatch(text)
+    mismatch = find_mismatch(text) #vai ievaditais satur i burtu , ja satur tad sak izpildit sakot no 35 rindas
     # Printing answer, write your code here
 
 
