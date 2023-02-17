32d255fc86e4dbfe13f63fc6f76be94726dd44f7
0931REB462 <125568233+0931REB462@users.noreply.github.com>
2023-02-17 10:49:28 +0000

    -

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# python3
+# python3 Aleksejs Kondratjevs 091REB462
 
 from collections import namedtuple
-from turtle import position
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
@@ -16,12 +15,12 @@ def find_mismatch(text):
         # enumerate - skaitītājs, simbols
         if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next, position + 1))
+            opening_brackets_stack.append(Bracket(next, i + 1))
             
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1], next):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 print(i+1)
             opening_brackets_stack.pop()
     if not opening_brackets_stack:
@@ -33,9 +32,12 @@ def find_mismatch(text):
 
 def main():
     text = input()
-    mismatch = find_mismatch(text) #vai ievaditais satur i burtu , ja satur tad sak izpildit sakot no 35 rindas
     # Printing answer, write your code here
-
+    if "I" in text:
+        text = input()
+        find_mismatch(text) #vai ievaditais satur i burtu , ja satur tad sak izpildit sakot no 35 rindas
+        # if mismatch == 0:
+        #     print("Success")
 
 if __name__ == "__main__":
     main()
